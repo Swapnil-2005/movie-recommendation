a652524759883b1a306dbc7af9176243d361f3a5
Swapnil-2005 <swapnilbanerjee183@gmail.com>
2026-02-15 16:04:42 +0530

    streamlit link added for backend api

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # =============================
 # CONFIG
 # =============================
-API_BASE = os.getenv("API_BASE", "http://127.0.0.1:8000")
+API_BASE =  "https://movie-recommendation-vzh6.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(
